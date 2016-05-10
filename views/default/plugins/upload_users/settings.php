2fd4e0a64ed67967dbe284ada06619e518f9111b
Florian DANIEL ~ Facyla <i.daniel.florian@gmail.com>
2016-05-10 13:35:18 +0200

    Inline html

--- views/default/plugins/upload_users/settings.php
+++ views/default/plugins/upload_users/settings.php
@@ -1,10 +1,18 @@
 <?php
-
 // Custom message in email notification
-echo '<p><label>' . elgg_echo('upload_users:notification:custom'). '</label> ' . elgg_view('input/longtext', array('name' => 'params[custom_message]', 'value' => $vars['entity']->custom_message)) . '</p>';
 
-
-// Preview message
-echo '<h3>' . elgg_echo('upload_users:notification:preview'). '</h3>';
+// Preview notification message
 $preview_message = elgg_echo('upload_users:email:message', array("[NAME]", elgg_get_config('sitename'), "[USERNAME]", "[PASSWORD]", elgg_get_site_url(), $vars['entity']->custom_message));
-echo '<blockquote>' . nl2br($preview_message) . '</blockquote>';
+?>
+
+<div>
+	<p>
+		<label><?php echo elgg_echo('upload_users:notification:custom'); ?></label>
+		<?php echo elgg_view('input/longtext', array('name' => 'params[custom_message]', 'value' => $vars['entity']->custom_message)); ?>
+	</p>
+</div>
+
+<div>
+	<h3><?php echo elgg_echo('upload_users:notification:preview'); ?></h3>
+	<blockquote><?php echo nl2br($preview_message); ?></blockquote>
+</div>
